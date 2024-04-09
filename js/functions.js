fdbc4d943753338dbc90f9037d29ad5a06d5b6f8
MattiaMessana <mattia.messana@icloud.com>
2024-04-09 17:12:37 +0200

    add: milestone 2

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,45 @@
 //MILESTONE 1
+function numberClik() { 
+    const number = this.innerText;
+    const result = document.getElementById('result-display');
+    if (result.innerHTML === "0") {
+        result.innerHTML = number;
+    } else {
+        result.innerHTML += number
+    }
+    
+}
+
+//MILESTONE 2 
+/**
+ * Description  salvare l'operatore ed il numero in un arrey 
+ * @returns {array} 
+ */
+function operationCLik() {
+    console.log("operationCLik", this.innerText);
+    const result = document.getElementById('result-display');
+    operation[0] = result.innerHTML;
+    operation[1] = this.innerText;
+    result.innerHTML = 0;
+    console.log(operation);
+}
+
+//MILESTONE 3 
+/**
+ * Description salvare il secondo operando ed effettuare il calcolo 
+ * @returns {number}
+ */
+function calc() {
+    const result = document.getElementById('result-display');   
+}
+
+
+
+
+//rest 
+function reset () {
+    const result = document.getElementById('result-display');
+    result.innerHTML = "0";
+}
+
+
--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-//individuiamo il contenier del risultato dell'operazione 
-const displayNum = document.querySelector("span");
-console.log(displayNum);
+//variabili globali 
+const operation = [];
 
-//indeividuiamo i numeri in paagina 
+//indeividuiamo i numeri in pagina 
 const nums = document.querySelectorAll(".number");
 
+//creiamo ciclo for per selezionare tutti i numeri 
 for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
+    num.addEventListener("click" , numberClik); 
 
-    num.addEventListener("click" , function() {
-        displayNum.innerHTML += num.innerHTML;
-    });
+};
+
+//individuare gli operatori in pagina 
+const ops = document.querySelectorAll(".op-item");
+
+//creiamo ciclo for per selezionare gli operatori 
+for (let i = 0 ; i < ops.length ; i++) {
+    const op = ops[i];
+    op.addEventListener('click' , operationCLik );
 }
 
+//cliccando sull'uguale si effettua il calcolo 
+const equal = document.querySelector(".equal");
+for (let i = 0 ; i < equal.length ; i++){
+    equal.addEventListener('click', calc)
+}
 
+//cliccando sul pulsante canc si resetta il calcolo 
+const canc = document.querySelector(".canc");
+for ( let i = 0 ; i < canc.length ; i++) {
+    canc.addEventListener('click', reset);
+}
